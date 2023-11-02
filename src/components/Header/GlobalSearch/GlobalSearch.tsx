5ef28e496d47834deadb269667ead82239e6c26c
awesomex <bestawes0mex@gmail.com>
2023-11-02 19:04:39 +0600

    header pc done

---
+++ src/components/Header/GlobalSearch/GlobalSearch.tsx
@@ -0,0 +1,33 @@
+import { ChangeEvent } from 'react'
+import { useAppDispatch, useAppSelector } from '../../../custom/hooks'
+import {
+	selectGlobalSearch,
+	setGlobalSearch,
+} from '../../../state/Slices/MainPageSlice/MainPageSliceData'
+import styles from './GlobalSearch.module.scss'
+import { AiOutlineSearch } from 'react-icons/ai'
+
+const GlobalSearch = () => {
+	const searchValue = useAppSelector(selectGlobalSearch)
+	const dispatch = useAppDispatch()
+	const changeSearchValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		dispatch(setGlobalSearch(e.target.value))
+	}
+	return (
+		<div className={styles.globalSearch}>
+			<AiOutlineSearch className={styles.statickSearchLogo} />
+			<input
+				value={searchValue}
+				onChange={changeSearchValueHandler}
+				placeholder='Поиск по сайту'
+				className={styles.searchInput}
+				type='text'
+			/>
+			{searchValue.trim() && (
+				<AiOutlineSearch className={styles.searchActive} />
+			)}
+		</div>
+	)
+}
+
+export default GlobalSearch
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,13 @@
 import { FC } from 'react'
+import { FaPhoneVolume } from 'react-icons/fa6'
+import { BsInstagram, BsTelegram, BsWhatsapp } from 'react-icons/bs'
+
 import styles from './Header.module.scss'
 import Hamburger from './Hamburger/Hamburger'
 import LanguageVariant from './LanguageVariant/LanguageVariant'
 import logo from '../../assets/logo.png'
+
+import GlobalSearch from './GlobalSearch/GlobalSearch'
 
 const Header: FC = () => {
 	return (
@@ -17,10 +22,38 @@ const Header: FC = () => {
 						</a>
 					</div>
 				</div>
-				<div className={styles.workPhone}>yo</div>
-				<div>
-					<div className={styles.globalSearch}>yo</div>
-					<div className={styles.socialContacts}></div>
+				<div className={styles.workPhone}>
+					<div className={styles.phoneIcon}>
+						<FaPhoneVolume />
+					</div>
+					<div className={styles.contactContent}>
+						<span>Работаем с 10:00 до 19:00</span>
+						<a className={styles.contactLink} href='tel:+77007007700'>
+							+7 (700) 700-77-00
+						</a>
+					</div>
+				</div>
+				<div className={styles.searchContactsWrapper}>
+					<GlobalSearch />
+					<div className={styles.socialContacts}>
+						<ul className={styles.socialWrapper}>
+							<li title='Наш инстаграм!'>
+								<a href='#'>
+									<BsInstagram />
+								</a>
+							</li>
+							<li title='Наш WhatsApp!'>
+								<a href='#'>
+									<BsWhatsapp />
+								</a>
+							</li>
+							<li title='Наш телеграм!'>
+								<a href='#'>
+									<BsTelegram />
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</header>
